b618f1a28c026f0e0d8ac6d25209eae1c5a68dd5
george.petropoulos <george.petropoulos@upstreamsystems.com>
2022-04-03 22:19:56 +0300

    fix syntax

--- migrations/create_bets_table.sql
+++ migrations/create_bets_table.sql
@@ -5,7 +5,7 @@ CREATE TABLE bets
     prediction    VARCHAR(20)   NOT NULL,
     size          VARCHAR(100)  NOT NULL,
     result        VARCHAR(10)   NOT NULL,
-    created_at    TIMESTAMP     DEFAULT NOW(),
+    created_at    TIMESTAMP     DEFAULT NOW()
 );
 
 CREATE INDEX idx_bets_date ON bets (created_at);
