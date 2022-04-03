e4abd0ef986ff209dab8dea396d9170d246bf9ab
george.petropoulos <george.petropoulos@upstreamsystems.com>
2022-04-03 20:37:22 +0300

    restructure

--- handlers/messages.go
+++ handlers/messages.go
@@ -2,12 +2,10 @@ package handlers
 
 import (
 	"fmt"
-	"regexp"
 	"strconv"
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
-	"github.com/glenn-brown/golang-pkg-pcre/src/pkg/pcre"
 )
 
 var messageConfig *MessageInfo
@@ -16,15 +14,6 @@ type MessageInfo struct {
 	ChannelID string
 	UserID    string
 }
-
-var (
-	betRegexp1  = regexp.MustCompile(`(.*?)(o|over|u|under)[0-9]{1,2}([.]5)? [0-9]{1,3}u(.*)`)
-	betRegexp2  = regexp.MustCompile(`(.*?)[0-9]{1,3}u (o|over|u|under)[0-9]{1,2}([.]5)?(.*)`)
-	betRegexp3  = regexp.MustCompile(`(.*?)(o|over|u|under)[0-9]{1,2}([.]5)?(.*?)[0-9]{1,3}u(.*)`)
-	betRegexp4  = regexp.MustCompile(`(.*?)[0-9]{1,3}u(.*?)(o|over|u|under)[0-9]{1,2}([.]5)?(.*)`)
-	goalRegexp  = pcre.MustCompile(`([0-9])\1{2,}$`, 0)
-	unitsRegexp = regexp.MustCompile(`^[0-9]{1,3}u(.*?)`)
-)
 
 func MessageConfigInit(channel string, user string) {
 	messageConfig = &MessageInfo{
--- handlers/utils.go
+++ handlers/utils.go
@@ -1,5 +1,20 @@
 package handlers
 
+import (
+	"regexp"
+
+	"github.com/glenn-brown/golang-pkg-pcre/src/pkg/pcre"
+)
+
+var (
+	betRegexp1  = regexp.MustCompile(`(.*?)(o|over|u|under)[0-9]{1,2}([.]5)? [0-9]{1,3}u(.*)`)
+	betRegexp2  = regexp.MustCompile(`(.*?)[0-9]{1,3}u (o|over|u|under)[0-9]{1,2}([.]5)?(.*)`)
+	betRegexp3  = regexp.MustCompile(`(.*?)(o|over|u|under)[0-9]{1,2}([.]5)?(.*?)[0-9]{1,3}u(.*)`)
+	betRegexp4  = regexp.MustCompile(`(.*?)[0-9]{1,3}u(.*?)(o|over|u|under)[0-9]{1,2}([.]5)?(.*)`)
+	goalRegexp  = pcre.MustCompile(`([0-9])\1{2,}$`, 0)
+	unitsRegexp = regexp.MustCompile(`^[0-9]{1,3}u(.*?)`)
+)
+
 func isBet(content string) bool {
 	return betRegexp1.MatchString(content) || betRegexp2.MatchString(content) || betRegexp3.MatchString(content) || betRegexp4.MatchString(content)
 }
