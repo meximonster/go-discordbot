35a31c66d782ca88b62a6a6726193dd697ae6b61
george.petropoulos <george.petropoulos@upstreamsystems.com>
2022-04-06 01:43:43 +0300

    change default timezone to athens

--- migrations/create_bets_table.sql
+++ migrations/create_bets_table.sql
@@ -6,7 +6,7 @@ CREATE TABLE bets
     size          INTEGER  NOT NULL,
     odds          NUMERIC,
     result        VARCHAR(10)   NOT NULL,
-    posted_at     TIMESTAMP     DEFAULT NOW()
+    posted_at     TIMESTAMP     DEFAULT (now() at time zone 'utc-3')
 );
 
 CREATE INDEX idx_bets_date ON bets (posted_at);
