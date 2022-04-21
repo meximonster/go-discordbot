3a18f445222ab4a44650f94eeb429734eee61b2a
george.petropoulos <george.petropoulos@upstreamsystems.com>
2022-04-21 16:34:14 +0300

    fix meme response

--- random/meme.go
+++ random/meme.go
@@ -10,15 +10,19 @@ type Meme struct {
 	Url      string `json:"url"`
 }
 
+type MemeResponse struct {
+	Memes []Meme `json:"memes"`
+}
+
 func GetRandomMeme() (string, string, error) {
 	r, err := cl.Get("https://meme-api.herokuapp.com/gimme/1")
 	if err != nil {
 		return "", "", fmt.Errorf("error during request: %s", err.Error())
 	}
 	defer r.Body.Close()
-	var m Meme
+	var m MemeResponse
 	if err := json.NewDecoder(r.Body).Decode(&m); err != nil {
 		return "", "", fmt.Errorf("error decoding response: %s", err.Error())
 	}
-	return m.PostLink, m.Url, nil
+	return m.Memes[0].PostLink, m.Memes[0].Url, nil
 }
