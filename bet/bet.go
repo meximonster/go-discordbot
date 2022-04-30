9164979d54fa21cdfa531c35a03a07c06d5753fe
george.petropoulos <george.petropoulos@upstreamsystems.com>
2022-04-30 17:04:38 +0300

    enhance regexp

--- bet/bet.go
+++ bet/bet.go
@@ -9,8 +9,8 @@ import (
 )
 
 var (
-	betRegexp1       = regexp.MustCompile(`(.*?)((o|over|u|under|\+|\-)[0-9]{1,2}([.]2?7?5)?|X|x|1|2|1X|1x|2x|2X|X2|x2)(.*?)[0-9]{1,3}u(.*)`)
-	betRegexp2       = regexp.MustCompile(`(.*?)[0-9]{1,3}u(.*?)((o|over|u|under|\+|\-)[0-9]{1,2}([.]2?7?5)?|X|x|1|2|1X|1x|2x|2X|X2|x2)(.*)`)
+	betRegexp1       = regexp.MustCompile(`(.*?)((o|over|u|under|\+|\-)[0-9]{1,2}([.]2?7?5)?|X|x|1|2|1X|1x|2x|2X|X2|x2|combo|[0-9]ada)(.*?)[0-9]{1,3}u(.*)`)
+	betRegexp2       = regexp.MustCompile(`(.*?)[0-9]{1,3}u(.*?)((o|over|u|under|\+|\-)[0-9]{1,2}([.]2?7?5)?|X|x|1|2|1X|1x|2x|2X|X2|x2|combo|[0-9]ada)(.*)`)
 	unitsRegexp      = regexp.MustCompile(`^[0-9]{1,3}u(.*?)$`)
 	predictionRegexp = regexp.MustCompile(`^((o|over|u|under|\+|\-)[0-9]{1,2}([.]2?7?5)?(ck)?|X|x|1|2|1X|1x|2x|2X|X2|x2|combo|[0-9]ada)$`)
 	oddsRegexp       = regexp.MustCompile(`^@([0-9]*[.])?[0-9]+$`)
