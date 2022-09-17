7095cbc4e179da7b2628149ce5e6c3ea7157f5ab
meximonster <tiltaki@gmail.com>
2022-09-17 15:03:02 +0300

    change queries

--- bet/parse.go
+++ bet/parse.go
@@ -5,6 +5,41 @@ import (
 	"strings"
 )
 
+var monthCase string = `CASE WHEN month = '01' then 'Jan' WHEN month = '02' then 'Feb' WHEN month = '03' then 'Mar' 
+WHEN month = '04' then 'Apr' WHEN month = '05' then 'May' WHEN month = '06' then 'Jun' 
+WHEN month = '07' then 'Jul' WHEN month = '08' then 'Aug' WHEN month = '09' then 'Sep' 
+WHEN month = '10' then 'Oct' WHEN month = '11' then 'Nov' ELSE 'Dec' END AS month`
+var sizeCase = `CASE WHEN size BETWEEN 1 AND 4 THEN '1-4' 
+WHEN size BETWEEN 5 AND 9 THEN '5-9' WHEN size = 10 then '10' WHEN size BETWEEN 11 AND 15 THEN '11-15' WHEN SIZE BETWEEN 16 AND 25 
+THEN '16-25' ELSE '25+' END`
+var unitPerMonthQuery = `SELECT units, ` + monthCase + ` 
+FROM (SELECT sum(CASE WHEN result = 'won' THEN size*odds - size ELSE -size END) as units, to_char(posted_at, 'mm') as month 
+FROM <table> group by 2 order by 2) foo;`
+var betsPerMonthQuery = `SELECT bets, ` + monthCase + ` 
+FROM (select count(1) as bets, to_char(posted_at, 'mm') as month 
+FROM <table> group by 2 order by 2) foo;`
+var percentPerSizeQuery = `SELECT CAST((CAST(won_bets AS DECIMAL(7,2)) / total_bets) * 100 AS DECIMAL(5,2)) as percentage, size, total_bets AS bets FROM 
+(SELECT * FROM (SELECT count(1) as total_bets, ` + sizeCase + ` AS size FROM <table> GROUP BY 2) a 
+INNER JOIN 
+(SELECT count(1) as won_bets, ` + sizeCase + ` as won_size FROM <table> where result = 'won' GROUP BY 2) b 
+ON a.size = b.won_size) c ORDER BY percentage desc;`
+var overQuery = `select count(1) FROM <table> where prediction like 'o%' 
+and prediction not like '%ck%' and result = 'won' 
+UNION 
+select count(1) FROM <table> where prediction like 'o%' and prediction not like '%ck%';`
+var ckQuery = `select count(1) FROM <table> where prediction like '%ck%' and result = 'won' 
+UNION 
+select count(1) FROM <table> where prediction like '%ck%';`
+var comboQuery = `select count(1) FROM <table> where prediction like '%combo%' and result = 'won' UNION select count(1) FROM <table> where prediction like '%combo%';`
+var hcQuery = `select count(1) FROM <table> where result = 'won' and prediction not like '%ck%' and prediction not like 'o%' and prediction not like '%combo%' 
+UNION 
+select count(1) FROM <table> where prediction not like '%ck%' and prediction not like 'o%' and prediction not like '%combo%';`
+var typeQueries = []string{overQuery, ckQuery, comboQuery, hcQuery}
+var countBySizeQuery = `select count(1) AS bets, ` + sizeCase + ` AS units FROM <table> group by 2 order by 1;`
+var countByTypeQuery = `select count(1) AS bets, CASE WHEN prediction like '%ck%' 
+THEN 'ck' WHEN prediction like 'o%' THEN 'over' WHEN prediction like '%combo%' THEN 'combo' 
+ELSE 'pregame/hc' END AS type FROM <table> group by 2 order by 1;`
+
 func Parse(content string, table string) string {
 	q := strings.Replace(content, "!bet ", "", 1)
 	args := dateParser(q)
@@ -29,4 +64,8 @@ func dateParser(q string) string {
 		args = q
 	}
 	return args
+}
+
+func parseGraphQuery(query string, table string) string {
+	return strings.ReplaceAll(query, "<table>", table)
 }
--- bet/repository.go
+++ bet/repository.go
@@ -2,47 +2,11 @@ package bet
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/jmoiron/sqlx"
 )
 
 var dbC *sqlx.DB
-
-var monthCase string = `CASE WHEN month = '01' then 'Jan' WHEN month = '02' then 'Feb' WHEN month = '03' then 'Mar' 
-WHEN month = '04' then 'Apr' WHEN month = '05' then 'May' WHEN month = '06' then 'Jun' 
-WHEN month = '07' then 'Jul' WHEN month = '08' then 'Aug' WHEN month = '09' then 'Sep' 
-WHEN month = '10' then 'Oct' WHEN month = '11' then 'Nov' ELSE 'Dec' END AS month`
-var sizeCase = `CASE WHEN size BETWEEN 1 AND 4 THEN '1-4' 
-WHEN size BETWEEN 5 AND 9 THEN '5-9' WHEN size = 10 then '10' WHEN size BETWEEN 11 AND 15 THEN '11-15' WHEN SIZE BETWEEN 16 AND 25 
-THEN '16-25' ELSE '25+' END`
-var unitPerMonthQuery = `SET TIMEZONE='Europe/Athens'; SELECT units, ` + monthCase + ` 
-FROM (SELECT sum(CASE WHEN result = 'won' THEN size*odds - size ELSE -size END) as units, to_char(posted_at, 'mm') as month 
-FROM <table> group by 2 order by 2) foo;`
-var betsPerMonthQuery = `SET TIMEZONE='Europe/Athens'; SELECT bets, ` + monthCase + ` 
-FROM (select count(1) as bets, to_char(posted_at, 'mm') as month 
-FROM <table> group by 2 order by 2) foo;`
-var percentPerSizeQuery = `SELECT CAST((CAST(won_bets AS DECIMAL(7,2)) / total_bets) * 100 AS DECIMAL(5,2)) as percentage, size, total_bets AS bets FROM 
-(SELECT * FROM (SELECT count(1) as total_bets, ` + sizeCase + ` AS size FROM <table> GROUP BY 2) a 
-INNER JOIN 
-(SELECT count(1) as won_bets, ` + sizeCase + ` as won_size FROM <table> where result = 'won' GROUP BY 2) b 
-ON a.size = b.won_size) c ORDER BY percentage desc;`
-var overQuery = `select count(1) FROM <table> where prediction like 'o%' 
-and prediction not like '%ck%' and result = 'won' 
-UNION 
-select count(1) FROM <table> where prediction like 'o%' and prediction not like '%ck%';`
-var ckQuery = `select count(1) FROM <table> where prediction like '%ck%' and result = 'won' 
-UNION 
-select count(1) FROM <table> where prediction like '%ck%';`
-var comboQuery = `select count(1) FROM <table> where prediction like '%combo%' and result = 'won' UNION select count(1) FROM <table> where prediction like '%combo%';`
-var hcQuery = `select count(1) FROM <table> where result = 'won' and prediction not like '%ck%' and prediction not like 'o%' and prediction not like '%combo%' 
-UNION 
-select count(1) FROM <table> where prediction not like '%ck%' and prediction not like 'o%' and prediction not like '%combo%';`
-var typeQueries = []string{overQuery, ckQuery, comboQuery, hcQuery}
-var countBySizeQuery = `select count(1) AS bets, ` + sizeCase + ` AS units FROM <table> group by 2 order by 1;`
-var countByTypeQuery = `select count(1) AS bets, CASE WHEN prediction like '%ck%' 
-THEN 'ck' WHEN prediction like 'o%' THEN 'over' WHEN prediction like '%combo%' THEN 'combo' 
-ELSE 'pregame/hc' END AS type FROM <table> group by 2 order by 1;`
 
 func NewDB(db *sqlx.DB) {
 	dbC = db
@@ -81,7 +45,7 @@ func GetBetsSumByQuery(query string) ([]BetSummary, error) {
 }
 
 func GetUnitsPerMonth(table string) ([]UnitsPerMonth, error) {
-	q := strings.ReplaceAll(unitPerMonthQuery, "<table>", table)
+	q := parseGraphQuery(unitPerMonthQuery, table)
 	r := []UnitsPerMonth{}
 	err := dbC.Select(&r, q)
 	if err != nil {
@@ -91,7 +55,7 @@ func GetUnitsPerMonth(table string) ([]UnitsPerMonth, error) {
 }
 
 func GetBetsPerMonth(table string) ([]BetsPerMonth, error) {
-	q := strings.ReplaceAll(betsPerMonthQuery, "<table>", table)
+	q := parseGraphQuery(betsPerMonthQuery, table)
 	r := []BetsPerMonth{}
 	err := dbC.Select(&r, q)
 	if err != nil {
@@ -101,7 +65,7 @@ func GetBetsPerMonth(table string) ([]BetsPerMonth, error) {
 }
 
 func GetPercentBySize(table string) ([]PercentPerSize, error) {
-	q := strings.ReplaceAll(percentPerSizeQuery, "<table>", table)
+	q := parseGraphQuery(percentPerSizeQuery, table)
 	r := []PercentPerSize{}
 	err := dbC.Select(&r, q)
 	if err != nil {
@@ -111,7 +75,7 @@ func GetPercentBySize(table string) ([]PercentPerSize, error) {
 }
 
 func GetWonPerType(query string, table string) ([]float64, error) {
-	q := strings.ReplaceAll(query, "<table>", table)
+	q := parseGraphQuery(query, table)
 	r := make([]float64, 0, 2)
 	err := dbC.Select(&r, q)
 	if err != nil {
@@ -121,7 +85,7 @@ func GetWonPerType(query string, table string) ([]float64, error) {
 }
 
 func GetCountBySize(table string) ([]CountBySize, error) {
-	q := strings.ReplaceAll(countBySizeQuery, "<table>", table)
+	q := parseGraphQuery(countBySizeQuery, table)
 	r := []CountBySize{}
 	err := dbC.Select(&r, q)
 	if err != nil {
@@ -131,7 +95,7 @@ func GetCountBySize(table string) ([]CountBySize, error) {
 }
 
 func GetCountByType(table string) ([]CountByType, error) {
-	q := strings.ReplaceAll(countByTypeQuery, "<table>", table)
+	q := parseGraphQuery(countByTypeQuery, table)
 	r := []CountByType{}
 	err := dbC.Select(&r, q)
 	if err != nil {
