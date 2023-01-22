fb24fb36951c730e3025cdf729f4d2b0d554dc30
meximonster <tiltaki@gmail.com>
2023-01-22 15:52:46 +0200

    fix time diff

--- bet/parse.go
+++ bet/parse.go
@@ -44,23 +44,23 @@ a join (select sum(size) as units from %[1]v) b on 1=1;`
 func Parse(content string, table string) string {
 	q := strings.Replace(content, "!bet ", "", 1)
 	args := dateParser(q)
-	query := fmt.Sprintf("SET TIMEZONE='Europe/Athens'; SELECT * FROM %s WHERE ", table) + strings.ReplaceAll(args, " ", " AND ")
+	query := fmt.Sprintf("SELECT * FROM %s WHERE ", table) + strings.ReplaceAll(args, " ", " AND ")
 	return query
 }
 
 func ParseSum(content string, table string) string {
 	q := strings.Replace(content, "!betsum ", "", 1)
 	args := dateParser(q)
-	query := fmt.Sprintf("SET TIMEZONE='Europe/Athens'; SELECT count(1), sum(CASE WHEN result = 'won' THEN size*odds - size ELSE size END) as total_units, result FROM %s WHERE %s group by 3 order by 1", table, args)
+	query := fmt.Sprintf("SELECT count(1), sum(CASE WHEN result = 'won' THEN size*odds - size ELSE size END) as total_units, result FROM %s WHERE %s group by 3 order by 1", table, args)
 	return query
 }
 
 func dateParser(q string) string {
 	var args string
 	if strings.Contains(q, "date") {
-		args = strings.Replace(q, "date", "posted_at::date", 1)
+		args = strings.Replace(q, "date", "(posted_at - INTERVAL '1' HOUR)::date", 1)
 	} else if strings.Contains(q, "today") {
-		args = strings.Replace(q, "today", "posted_at::date=CURRENT_DATE", 1)
+		args = strings.Replace(q, "today", "(posted_at - INTERVAL '1' HOUR)::date=CURRENT_DATE", 1)
 	} else {
 		args = q
 	}
